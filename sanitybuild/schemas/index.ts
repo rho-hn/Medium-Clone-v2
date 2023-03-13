3a730504f768d1c0d9ec65081f0dcc8c12d8595e
Rohan Srivastava <rohan.srivastava@bytelearn.com>
2023-03-13 11:39:38 +0530

    removed sanitybuild file

--- sanitybuild/schemas/index.ts
+++
@@ -1,7 +0,0 @@
-import post from './post'
-import author from './author'
-import blockContent from './blockContent'
-import category from './category'
-import comment from './comment'
-
-export const schemaTypes = [post,author,blockContent,category,comment]
